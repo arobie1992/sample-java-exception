b792dde999833b3b689693c0091b8cc8fd6c7ca3
Andrew Robie <arobie1992@yahoo.com>
2022-02-14 22:21:38 -0500

    Example file

---
+++ Example.java
@@ -0,0 +1,36 @@
+import java.util.Math;
+
+public class Example {
+	
+	// tool flags this because it throws IAE without declaring it in a javadoc comment
+	public double posSqrt(double d) {
+		if(d < 0) {
+			throw new IllegalArgumentException("d must be >= 0");
+		}
+		return Math.sqrt(d);
+	}
+
+	// tool flags this because it calls a method that can throw IAE without either handling or specifying in javadoc
+	public void printSqrt(double d) {
+		System.out.println(d + "'s square root is " + posSqrt(d));
+	}
+
+	// tool doesn't flag this because it declares the throws in the javadoc and provides an explanation
+	// stretch goal could be to make sure the reason is accurate
+	/**
+         * @throws NullPointerException if {@code d} is {@code null}
+	 */
+	public Double posSqrt(Double d) {
+		if(d == null) {
+			throw new NullPointerException("d cannot be null");
+		}
+		return posSqrt(d);
+	}
+
+	// tool would flag this because posSqrt(Double) specifies @throws in its javadoc
+	// stretch goal would be to infer the reason for the @throws exception 
+	public void printSqrt(Double d) {
+		System.out.println(d + "'s square root is " + posSqrt(d));
+	}
+
+}
